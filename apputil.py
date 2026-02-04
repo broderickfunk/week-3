eff34ee2cff342cda9ac2b3e2a8bee1a9b1aafdf
Brody Funk <brodfunk@iu.edu>
2026-02-04 17:37:21 -0500

    commit

--- apputil.py
+++ apputil.py
@@ -1,5 +1,10 @@
-import seaborn as sns
-import pandas as pd
-
-
 # update/add code below ...
+def fibonacci(n):
+    """Return the nth number of the Fibonacci Series."""
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else: 
+        return fibonacci(n-1) + fibonacci(n-2)
+fibonacci(9)
