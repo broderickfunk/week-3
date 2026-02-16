5c082982b3c5cf44b53943733f9015bd2cc3e30b
Brody Funk <brodfunk@iu.edu>
2026-02-16 13:16:18 -0500

    Refactor tasks in apputil.py and add data processing

--- apputil.py
+++ apputil.py
@@ -18,26 +18,32 @@ def to_binary(n: int) -> str:
         return to_binary(n // 2) + str(n % 2)
 to_binary(2)
 ######
-def task_1():
-    # columns sorted least -> most missing values
+import pandas as pd
+url = 'https://github.com/melaniewalsh/Intro-Cultural-Analytics/raw/master/book/data/bellevue_almshouse_modified.csv'
+
+df_bellevue = pd.read_csv(url)
+df_bellevue["gender"] = df_bellevue["gender"].astype(str).str.strip().str.lower()
+df_bellevue.loc[~df_bellevue["gender"].isin(["m","w"]), "gender"] = pd.NA
+
+def task_i():
+    print("The gender column returned multiple values that I did not expect. " \
+    "I changed the letters not representing man or woman to missing because we cannot know what those values mean.")
     return df_bellevue.isna().sum().sort_values().index.tolist()
 
 
-def task_2():
-    # dataframe: year, total_admissions
+def task_ii():
+    print("For ease changing the data column to datetime format first was essential.")
     return (
         df_bellevue.assign(year=pd.to_datetime(df_bellevue["date_in"], errors="coerce").dt.year)
-                  .groupby("year")
-                  .size()
-                  .reset_index(name="total_admissions")
+        .groupby("year")
+        .size()
+        .reset_index(name="total_admissions")
     )
 
 
-def task_3():
-    # series: index=gender, values=avg age
+def task_iii():
     return df_bellevue.groupby("gender")["age"].mean()
 
 
-def task_4():
-    # list: 5 most common professions
+def task_iv():
     return df_bellevue["profession"].value_counts().head(5).index.tolist()
